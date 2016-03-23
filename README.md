e9e7f5a98f00d2af82cc14205d1cbb481e53a330
Neel Bommisetty <neel250294@gmail.com>
2016-03-23 10:32:58 +0530

    updated documentation, Added readme and fork me on github link

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# GitIssuesDashboard
-This application takes a git  repository and says number of open issues in that
+# Git Issues Dashboard
+Git dashboard is an application that takes a repository and makes a issues dashboard out of it, which contains all the issues created in one screen.
+
+[Click here for demo](http://githubissuesboard.herokuapp.com/)
+
+# Available Filters
+* Last 24 hours
+* 24 hours-7days
+* More than 7days
+
+These filters work based on the creation date(**not updation date**).
+
+# Technical Details
+This application works only with public repositories on github.com and uses github's public api to fetch the details and the whole application is made out of javascript.
+
+# Possible Extenstions
+1) Proper Validation of github repository links
+2) Use pagination where ever required (now only fetches first 100 open issues).
+3) login with github and explore issues of all your repositories in one place
+4) enabling show and add comments on the same page under the issue itself.
+
+There are many more possiblities to extend it.
--- static/js/index.js
+++ static/js/index.js
@@ -1,22 +1,32 @@
 $(function () {
 	'use strict';
+	$(".dataRow").hide();
+	//submit click handler 
 	$('[data-action="submit"]').click( function (e) {
 		e.preventDefault();
+
+		//parsing the url for owner name and repository name
 		var link = $('#repoLink').val();
+		//empty input handling
 		if(link === ""){
 			Materialize.toast('Please fill in the url and press submit', 5000, 'danger');
 			return;
 		}
+
+		//stripping unncessary things from url
 		link =link.replace("https://","");
 		link =link.replace("http://","");
 		link =link.replace(".git","");
 		var repoOwner = link.split("/")[1];
 		var repoName = link.split("/")[2];
-		if(link.split("/")[0].indexOf("github") <0){
+
+		//checking if repository is from github (hack,better solutions exist!) 
+		if(link.split("/")[0].indexOf("github.com") <0){
 			Materialize.toast('Only works with github.com repos', 5000, 'danger');
 			return;
 		}
 
+		//helper variables and functions for rendering fetched data
 		var last24Hours=moment().subtract(1,'day');
 		var last7DayEnd = moment().subtract(7,'day');
 		function setHtml(selector,data) {
@@ -27,29 +37,41 @@ $(function () {
 		    html +=	"</ul>";
 		    $(selector).html(html);	
 		};
+
+		//getting the data from github puclic api.
 		$.get('https://api.github.com/repos/'+repoOwner+"/"+repoName+"/issues?state=open&per_page=100" ,function (data){
+			$(".dataRow").show();
+			$('.repoTitle').html(repoName);
+
+			//removing pull requests as github treats pull requests as issues as well
 			var allData =_.filter(data,function(item){
 				return !item.pull_request ;
 			});
-			$('.all').html('All('+allData.length+")");
+
+			//rendering all the data in all tab
+			$('.all').html('All ('+allData.length+")");
 			setHtml("#all",allData);
+
+			//filtering data for different time frames and rendering the data
 			var last24hoursData =_.filter(allData,function(item){
 				return moment(item.created_at).isAfter(last24Hours);
 			});
-			$('.24h').html('Last 24 hours('+last24hoursData.length+")");
+			$('.24h').html('Last 24 hours ('+last24hoursData.length+")");
 			setHtml("#24h",last24hoursData);
+
 			var lastWeekData =_.filter(allData,function(item){
 				return moment(item.created_at).isBefore(last24Hours) && moment(item.created_at).isAfter(last7DayEnd);
 			});
-			$('.7d').html('Last 7 days('+lastWeekData.length+")");
+			$('.7d').html('Last 7 days ('+lastWeekData.length+")");
 			setHtml("#7d",lastWeekData);
+
 			var greaterThanWeekData =_.filter(allData,function(item){
 				return moment(item.created_at).isBefore(last7DayEnd);
 			});
 			$('.gt7d').html("More Than 7 days Older ("+greaterThanWeekData.length+")");
 			setHtml("#gt7d",greaterThanWeekData);
 			
-		}).fail( function(){
+		}).fail( function(){  //handling error from api.
 			Materialize.toast('Some error occured please try again later!', 5000, 'danger');
 		});
 	});
